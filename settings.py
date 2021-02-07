fcdf43a35e4d51733e6853bcb1978fd7f871e64f
果汁 <71187213+FristyFlakes@users.noreply.github.com>
2021-02-07 22:03:24 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -29,7 +29,7 @@ send_image_track_query = "🎧 Track: %s \n👤 Artist: %s \n💽 Album: %s \n�
 send_image_album_query = "💽 Album: %s \n👤 Artist: %s \n📅 Date: %s \n🎧 Tracks amount: %d"
 send_image_artist_query = "👤 Artist: %s \n💽 Album numbers: %d \n👥 Fans on Deezer: %d"
 tags_query = "💽 Album: %s\n📅 Date: %s\n📀 Label: %s\n🎵 Genre: %s"
-info_msg = "🔺 Version: %s\n🔻 Name: @%s\n✒️ Creator: @%s\n💵 Donation: %s\n📣 Forum: %s\n👥 Users: %d\n⬇️ Total downloads: %d"
+info_msg = "🔺 Version: %s\n🔻 Bot Name: @%s\n🤖 Support Bot: @%s\n📢 Support Channel: %s\n🧕 Created By: %s\n👥 Total Users: %d\n⬇️ Total Downloads: %d"
 send_image_playlist_query = "📅 Creation: %s \n👤 User: %s \n🎧 Tracks amount: %d"
 insert_query = "INSERT INTO DWSONGS (id, query, quality) values ('%s', '%s', '%s')"
 where_query = "SELECT query FROM DWSONGS WHERE id = '{}' and quality = '{}'"
